d04fc86640d27ae78b0c868a515b675c2caa9994
kio <kio@little-bat.de>
2023-09-30 10:00:54 +0200

    Canvas: unit tests stubs

---
+++ Graphics/Canvas_unit_test.cpp
@@ -0,0 +1,60 @@
+// Copyright (c) 2023 - 2023 kio@little-bat.de
+// BSD-2-Clause license
+// https://opensource.org/licenses/BSD-2-Clause
+
+#include "Canvas.h"
+#include "Pixmap.h"
+#include "basic_math.h"
+#include "doctest.h"
+#include "stb/stb_image.h"
+#include <memory>
+
+using namespace kio::Graphics;
+using namespace kio;
+
+
+TEST_CASE("Canvas: is_inside()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: setPixel(), getPixel(), getColor(), getInk()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: drawHLine()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: drawVLine()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: fillRect()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: xorRect()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: clear()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: copyRect() inside Pixmap") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: copyRect() between Pixmaps") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: readBmp()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: drawBmp()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: drawChar()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: drawLine()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: drawRect()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: drawCircle()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: fillCircle()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: floodFill()") { CHECK(1 == 1); }
+
+TEST_CASE("Canvas: drawPolygon()") { CHECK(1 == 1); }
+
+
+#if 0 
+TEST_CASE("")
+{
+	REQUIRE(1 == 1);
+	CHECK(1 == 1);
+	SUBCASE("") {}
+}
+#endif
