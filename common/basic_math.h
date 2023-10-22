adff02a997216d886d13379c020ab0db52bd2b4d
kio <kio@little-bat.de>
2023-10-22 21:05:03 +0200

    basic_math: fix abs() & map_range()

--- common/basic_math.h
+++ common/basic_math.h
@@ -3,6 +3,7 @@
 // https://opensource.org/licenses/BSD-2-Clause
 
 #pragma once
+#include "standard_types.h"
 
 
 // basic math
@@ -45,6 +46,10 @@ inline constexpr T abs(T a) noexcept
 {
 	return a < 0 ? -a : a;
 }
+inline constexpr uint8	abs(int8 a) noexcept { return a < 0 ? uint8(-a) : uint8(a); }
+inline constexpr uint16 abs(int16 a) noexcept { return a < 0 ? uint16(-a) : uint16(a); }
+inline constexpr uint32 abs(int32 a) noexcept { return a < 0 ? uint32(-a) : uint32(a); }
+inline constexpr uint64 abs(int64 a) noexcept { return a < 0 ? uint64(-a) : uint64(a); }
 
 template<class T>
 inline constexpr T minmax(T a, T n, T e) noexcept
@@ -62,13 +67,13 @@ inline void limit(T a, T& n, T e) noexcept
 }
 
 template<typename T, typename T2>
-inline T2 map_range(T value, T qmax, T2 zmax)
+inline constexpr T2 map_range(T value, T qmax, T2 zmax)
 {
-	return T(float(value) / qmax * zmax + 0.5f);
+	return T2(float(value) / float(qmax) * float(zmax) + 0.5f);
 }
 
 template<>
-inline float map_range(float value, float qmax, float zmax)
+inline constexpr float map_range(float value, float qmax, float zmax)
 {
 	return value / qmax * zmax;
 }
