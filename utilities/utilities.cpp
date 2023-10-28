77e7e6b99aeb1943545d4f2e3bdab2635ee06483
kio <kio@little-bat.de>
2023-10-28 17:23:33 +0200

    utilities: fix init_stack_guard()

--- utilities/utilities.cpp
+++ utilities/utilities.cpp
@@ -158,12 +158,13 @@ void init_stack_guard()
 {
 	// fill free stack of current core with magic number:
 
-	char tmp;
-	ptr	 e = &tmp;
-	ptr	 p = stack_bottom(get_core_num());
+	uint tmp = 0xa5a5a5a5;
+	ptr	 e	 = ptr(&tmp);
+	ptr	 p	 = stack_bottom(get_core_num());
 	assert(uint(e - p) < 4 kB); // stack pages are 4k
 
-	memset(p, 0xe5, uint(e - p));
+	// don't use memset because the call to memset may push some values which are then overwritten!
+	for (; p < e; p += 4) { *reinterpret_cast<volatile uint*>(p) = 0xe5e5e5e5; }
 }
 
 void test_stack_guard(uint core)
