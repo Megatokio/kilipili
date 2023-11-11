7a9507cc5f9c707f06923f9ec1b21828e79263c4
kio <kio@little-bat.de>
2023-11-11 12:56:59 +0100

    SingleSprite: new VideoPlane for a single sprite. e.g. mouse pointer

---
+++ Video/SingleSprite.cpp
@@ -0,0 +1,161 @@
+// Copyright (c) 2023 - 2023 kio@little-bat.de
+// BSD-2-Clause license
+// https://opensource.org/licenses/BSD-2-Clause
+
+#include "SingleSprite.h"
+#include <pico/stdlib.h>
+#include <pico/sync.h>
+
+namespace kio::Video
+{
+using namespace Graphics;
+
+#define WRAP(X)	 #X
+#define XWRAP(X) WRAP(X)
+#define XRAM	 __attribute__((section(".scratch_x.spr" XWRAP(__LINE__))))		// the 4k page with the core1 stack
+#define RAM		 __attribute__((section(".time_critical.spr" XWRAP(__LINE__)))) // general ram
+
+
+spin_lock_t* singlesprite_spinlock = nullptr;
+
+
+#define lock_display_list() SingleSpriteLocker _locklist
+
+
+template<Animation ANIM, Softening SOFT>
+SingleSprite<ANIM, SOFT>::SingleSprite(const Shape shape, coord x, coord y) :
+	xpos(x),
+	ypos(y),
+	shape(shape),
+	hot_shape(nullptr),
+	frame_countdown(),
+	ghostly(false)
+{
+	if constexpr (ANIM == Animated)
+		if (!singlesprite_spinlock) singlesprite_spinlock = spin_lock_init(uint(spin_lock_claim_unused(true)));
+}
+
+template<Animation ANIM, Softening SOFT>
+SingleSprite<ANIM, SOFT>::SingleSprite(const Shape shape, const Point& p) : SingleSprite(shape, p.x, p.y)
+{}
+
+template<Animation ANIM, Softening SOFT>
+void SingleSprite<ANIM, SOFT>::setup(coord __unused width)
+{
+	hot_shape		= nullptr;
+	frame_countdown = shape.duration();
+}
+
+template<Animation ANIM, Softening SOFT>
+void RAM SingleSprite<ANIM, SOFT>::vblank() noexcept
+{
+	hot_shape = nullptr;
+
+	Shape hs;
+
+	if constexpr (ANIM == NotAnimated) { hs = shape; }
+	else
+	{
+		lock _;
+
+		hs = shape;
+		if (--frame_countdown <= 0)
+		{
+			shape = hs		= hs.next_frame();
+			frame_countdown = hs.duration() - 1;
+		}
+	}
+
+	int y = ypos - hs.preamble().hot_y;
+	if (y >= 0) return;
+
+	hot_shape_x = xpos - hs.preamble().hot_x;
+	hs.skip_preamble();
+	for (; y < 0; y++) // if sprite starts above screen:
+	{
+		assert(hs.is_pfx());
+		hot_shape_x += hs.dx(); // TODO Softened
+		hs.next_row();
+		if unlikely (hs.is_end()) return;
+	}
+	assert(hs.is_pfx());
+	hot_shape = hs;
+}
+
+template<Animation ANIM, Softening SOFT>
+void RAM SingleSprite<ANIM, SOFT>::renderScanline(int row, uint32* scanline) noexcept
+{
+	if (hot_shape.pixels == nullptr)
+	{
+		Shape s = shape;
+		if (row != ypos - s.preamble().hot_y) return;
+		hot_shape_x = xpos - s.preamble().hot_x;
+		s.skip_preamble();
+		assert(s.is_pfx());
+		hot_shape = s;
+	}
+
+	bool finished = hot_shape.render_one_row(hot_shape_x, reinterpret_cast<Color*>(scanline), ghostly);
+	if (finished) hot_shape = nullptr;
+}
+
+template<Animation ANIM, Softening SOFT>
+void SingleSprite<ANIM, SOFT>::wait_while_hot() const noexcept
+{
+	for (uint i = 0; i < 100000 / 60 && is_hot(); i++) { sleep_us(10); }
+}
+
+
+// the linker will know what we need:
+template class SingleSprite<NotAnimated, NotSoftened>;
+template class SingleSprite<NotAnimated, Softened>;
+template class SingleSprite<Animated, NotSoftened>;
+template class SingleSprite<Animated, Softened>;
+
+
+} // namespace kio::Video
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
---
+++ Video/SingleSprite.h
@@ -0,0 +1,113 @@
+// Copyright (c) 2023 - 2023 kio@little-bat.de
+// BSD-2-Clause license
+// https://opensource.org/licenses/BSD-2-Clause
+
+#pragma once
+#include "Shape.h"
+#include "VideoPlane.h"
+#include <pico/sync.h>
+
+namespace kio::Video
+{
+
+extern spin_lock_t* singlesprite_spinlock;
+
+/*
+	A VideoPlane for one single Sprite.
+	Intended for mouse pointer or player character.
+	
+	Variants per template:
+	- Animation = NotAnimated
+	- Animation = Animated
+	- Softening = NotSoftened
+	- Softening = Softened: 	 sprites are scaled 2:1 horizontally, odd pixels l+r become half transparent  ***TODO***
+	
+	Other options:
+	- ghostly:	  Shape can be rendered 50% transparent		
+*/
+template<Animation ANIM, Softening SOFT>
+class SingleSprite : public VideoPlane
+{
+	using Shape = Video::Shape<ANIM, SOFT>;
+	using coord = Graphics::coord;
+	using Point = Graphics::Point;
+
+	SingleSprite(const Shape, const Point& position);
+	SingleSprite(const Shape, coord x, coord y);
+
+	virtual void setup(coord width) override;
+	virtual void teardown() noexcept override {}
+	virtual void vblank() noexcept override;
+	virtual void renderScanline(int row, uint32* scanline) noexcept override;
+
+	coord xpos;
+	coord ypos;
+
+	bool is_hot() const noexcept { return hot_shape.pixels != nullptr; }
+
+	void move(coord x, coord y) noexcept { void(xpos = x), ypos = y; }
+	void move(const Point& p) noexcept { move(p.x, p.y); }
+
+	void modify(Shape s, coord x, coord y, bool wait_while_hot = 0) noexcept;
+	void modify(Shape s, const Point& p, bool wait_while_hot = 0) noexcept;
+	void replace(Shape s, bool wait_while_hot = 0) noexcept;
+
+private:
+	Shape shape; // the compressed image of the sprite.
+
+	Shape hot_shape;	   // pointer into shape as rows are displayed
+	int	  hot_shape_x;	   // updated x pos for current row
+	int16 frame_countdown; // animation
+	bool  ghostly;		   // render sprite semi transparent
+	char  _padding = 0;
+
+	void wait_while_hot() const noexcept;
+
+	struct lock
+	{
+		uint32 status_register;
+		lock() noexcept { status_register = spin_lock_blocking(singlesprite_spinlock); }
+		~lock() noexcept { spin_unlock(singlesprite_spinlock, status_register); }
+	};
+};
+
+
+// ============================================================================
+
+
+template<Animation ANIM, Softening SOFT>
+void SingleSprite<ANIM, SOFT>::replace(Shape s, bool wait) noexcept
+{
+	if constexpr (ANIM == NotAnimated) { shape = s; }
+	else
+	{
+		lock _;
+		shape			= s;
+		frame_countdown = s.duration();
+	}
+	if (wait) wait_while_hot(); // => caller can delete old shape safely
+}
+
+template<Animation ANIM, Softening SOFT>
+void SingleSprite<ANIM, SOFT>::modify(Shape s, const Point& new_p, bool wait) noexcept
+{
+	move(new_p);
+	replace(s, wait);
+}
+
+template<Animation ANIM, Softening SOFT>
+void SingleSprite<ANIM, SOFT>::modify(Shape s, coord new_x, coord new_y, bool wait) noexcept
+{
+	move(new_x, new_y);
+	replace(s, wait);
+}
+
+
+// yes, we have them all:
+extern template class SingleSprite<NotAnimated, NotSoftened>;
+extern template class SingleSprite<NotAnimated, Softened>;
+extern template class SingleSprite<Animated, NotSoftened>;
+extern template class SingleSprite<Animated, Softened>;
+
+
+} // namespace kio::Video
