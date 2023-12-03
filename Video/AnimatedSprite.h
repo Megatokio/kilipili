934730dd438c504337b0aa040b7d4d07e3cd485c
kio <kio@little-bat.de>
2023-12-03 18:56:00 +0100

    Sprite: cleanup template parameters, handle animation in AnimatedSprite, extract Frames

---
+++ Video/AnimatedSprite.h
@@ -0,0 +1,238 @@
+// Copyright (c) 2022 - 2023 kio@little-bat.de
+// BSD-2-Clause license
+// https://opensource.org/licenses/BSD-2-Clause
+
+#pragma once
+#include "Frames.h"
+#include "Sprite.h"
+#include "geometry.h"
+#include "string.h"
+
+namespace kio::Video
+{
+
+/* ————————————————————————————————————————————————————————————
+	Animated Sprite:
+	An AnimatedSprite is a Sprite with automatic animation.
+	VideoPlane SingleSpritePlane<> and MultiSpritePlane<> 
+	are based on any class Sprite which may be an AnimatedSprite.
+	
+	Because template class MultiSpritePlane<> is based on the exact Sprite class 
+	a MultiSpritePlane<AnimatedSprite> can only contain AnimatedSprites.
+	But a AnimatedSprite can consist of only one Frame, so a 
+	MultiSpritePlane<AnimatedSprite> can seemingly mix animated and not-animated Sprites.
+
+	An AnimatedSprite allocates one chunk on the heap, except for AnimatedSprites with only one Frame.
+	Replacing the Frames of an AnimatedSprite with new Frames results in deallocation and 
+	reallocation of this heap memory, except if old and new number of frames are the same.
+*/
+template<typename SHAPE>
+class AnimatedSprite : public Sprite<SHAPE>
+{
+public:
+	using super	 = Sprite<SHAPE>;
+	using Shape	 = SHAPE;
+	using Frame	 = Video::Frame<Shape>;
+	using Frames = Video::Frames<Shape>;
+
+	static constexpr bool is_animated = true; // helper for templates
+	static constexpr bool isa_sprite  = true;
+	static_assert(Shape::isa_shape);
+
+	using super::current_frame;
+	uint16 countdown = 0xffff;
+	Frames frames;
+
+	/*
+		create an AnimatedSprite from Frames<>.
+		The AnimatedSprite copies the data from the Frames. (does not take ownership of the vectors)
+		If frames.num_frames==1 then you should use `AnimatedSprite(Shape,x,y,z)` instead.
+	*/
+	AnimatedSprite(const Frames& frames, const Point&, uint16 z = 0) throws;
+	AnimatedSprite(Frames&& frames, const Point&, uint16 z = 0) throws;
+	AnimatedSprite(const Frame* frames, uint8 num_frames, const Point&, uint16 z = 0) throws;
+	AnimatedSprite(const Shape* shapes, const uint16* durations, uint8 num_frames, const Point& p, uint16 z = 0) throws;
+	AnimatedSprite(const Shape* shapes, uint16 duration, uint8 num_frames, const Point& p, uint16 z = 0) throws;
+
+	/*
+		create an AnimatedSprite with only one frame.
+		This makes no allocation on the heap and thus can't fail. (doesn't throw)
+	*/
+	AnimatedSprite(const Shape& shape, const Point& p, uint16 z = 0) noexcept; // ctor
+
+	~AnimatedSprite() noexcept = default;
+
+	/*
+		replace the Frames of this Sprite with a new set of Frames.
+		The AnimatedSprite copies the data from the vector. (does not take ownership)
+		If num_frames==1 then you should use `replace(Shape)` instead.
+		Returns true if hot_y changed and the Sprite may need re-linking.
+	*/
+	bool replace(const Frames& new_frames) throws;
+	bool replace(Frames&& new_frames) throws;
+	bool replace(const Frame* new_frames, uint new_num_frames) throws;
+	bool replace(const Shape* new_shapes, const uint16* new_durations, uint new_num_frames) throws;
+	bool replace(const Shape* new_shapes, uint16 new_duration, uint new_num_frames) throws;
+
+	/*
+		replace the Frames of this Sprite with a new single Shape and no animation.
+		This makes no allocation on the heap and thus can't fail. (doesn't throw)		
+		Returns true if hot_y changed and the Sprite may need re-linking.
+	*/
+	bool replace(const Shape& new_shape) noexcept;
+
+	/*
+		advance the animation to the next frame.
+		If the Sprite has currently only one frame then nothing happens,
+		else the new Shape and it's countdown are loaded.
+		Returns true if hot_y changed and the Sprite may need re-linking.
+	*/
+	bool next_frame() noexcept;
+};
+
+
+//
+// ============================ Implementations ================================
+//
+
+template<typename Shape>
+AnimatedSprite<Shape>::AnimatedSprite(const Frames& frames, const Point& p, uint16 z ) throws : // ctor
+	super(frames[0].shape, p, z),
+	countdown(frames[0].duration),
+	frames(frames)
+{}
+
+template<typename Shape>
+AnimatedSprite<Shape>::AnimatedSprite(Frames&& frames, const Point&p, uint16 z ) throws : // ctor
+	super(frames[0].shape, p, z),
+	countdown(frames[0].duration),
+	frames(std::move(frames))
+{}
+
+template<typename Shape>
+AnimatedSprite<Shape>::AnimatedSprite(const Frame* frames, uint8 num_frames, const Point&p, uint16 z ) throws : // ctor
+	super(frames[0].shape, p, z),
+	countdown(frames[0].duration),
+	frames(frames,num_frames)
+{}
+
+template<typename Shape>
+AnimatedSprite<Shape>::AnimatedSprite(
+	const Shape* shapes, const uint16* durations, uint8 num_frames, const Point& p, uint16 z) throws :
+	super(shapes[0], p, z),
+	countdown(durations[0]),
+	frames(shapes, durations, num_frames)
+{}
+
+template<typename Shape>
+AnimatedSprite<Shape>::AnimatedSprite(
+	const Shape* shapes, uint16 duration, uint8 num_frames, const Point& p, uint16 z) throws :
+	super(shapes[0], p, z),
+	countdown(duration),
+	frames(shapes, duration, num_frames)
+{}
+
+template<typename Shape>
+AnimatedSprite<Shape>::AnimatedSprite(const Shape& shape, const Point& p, uint16 z) noexcept : // ctor
+	super(shape, p, z)
+{}
+
+template<typename Shape>
+bool AnimatedSprite<Shape>::replace(const Frames& new_frames) throws
+{
+	frames.replace(new_frames);
+	current_frame = 0;
+	countdown	  = frames[0].duration;
+	return super::replace(frames[0].shape);
+}
+
+template<typename Shape>
+bool AnimatedSprite<Shape>::replace(Frames&& new_frames) throws
+{
+	frames.replace(std::move(new_frames));
+	current_frame = 0;
+	countdown	  = frames[0].duration;
+	return super::replace(frames[0].shape);
+}
+
+template<typename Shape>
+bool AnimatedSprite<Shape>::replace(const Frame* new_frames, uint new_num_frames) throws
+{
+	frames.replace(new_frames, new_num_frames);
+	current_frame = 0;
+	countdown	  = new_frames[0].duration;
+	return super::replace(new_frames[0].shape);
+}
+
+template<typename Shape>
+bool AnimatedSprite<Shape>::replace(const Shape* new_shapes, const uint16* new_durations, uint new_num_frames) throws
+{
+	frames.replace(new_shapes, new_durations, new_num_frames);
+	current_frame = 0;
+	countdown	  = frames[0].duration;
+	return super::replace(frames[0].shape);
+}
+
+template<typename Shape>
+bool AnimatedSprite<Shape>::replace(const Shape* new_shapes, uint16 new_duration, uint new_num_frames) throws
+{
+	frames.replace(new_shapes, new_duration, new_num_frames);
+	current_frame = 0;
+	countdown	  = new_duration;
+	return super::replace(frames[0].shape);
+}
+
+template<typename Shape>
+bool AnimatedSprite<Shape>::replace(const Shape& new_shape) noexcept
+{
+	frames.dealloc();
+	return super::replace(new_shape);
+}
+
+template<typename Shape>
+bool AnimatedSprite<Shape>::next_frame() noexcept
+{
+	if (frames.num_frames == 0) return false; // no animation
+	if (++current_frame >= frames.num_frames) current_frame = 0;
+	countdown = frames[current_frame].duration;
+	return super::replace(frames[current_frame].shape);
+}
+
+} // namespace kio::Video
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
--- Video/Sprite.h
+++ Video/Sprite.h
@@ -3,7 +3,7 @@
 // https://opensource.org/licenses/BSD-2-Clause
 
 #pragma once
-#include "Shape.h"
+#include "Frames.h"
 #include "geometry.h"
 #include "no_copy_move.h"
 #include <pico/sync.h>
@@ -15,23 +15,6 @@ extern volatile int hot_row;		  // the currently displayed screen row
 extern spin_lock_t* sprites_spinlock; // spinlock for all sprites
 
 
-/*
-	Sprites are ghostly images which hover above a regular video image.
-
-	Displaying sprites is quite cpu intensive.  
-	Eventually the most popular use of a sprite is the display of a mouse pointer.  
-	Sprites don't manage the lifetime of the shape => they don't delete it in ~Sprite().  
-
-	Shape must provide:
-		animated, 
-		width, height, hot_x, hot_y, 
-		next_frame(),
-		template<ZPlane> typedef HotShape
-	HotShape must provide:
-		skip_row()
-		render_row()
-*/
-
 struct LinkedListElement
 {
 	LinkedListElement* next = nullptr; // linking in the internal displaylist
@@ -39,138 +22,88 @@ struct LinkedListElement
 };
 
 
-/* ————————————————————————————————————————————————————————————
-	Not animated Sprite
-*/
-template<typename SHAPE, typename = void>
-class Sprite : protected LinkedListElement
-{
-	template<typename Sprite, ZPlane WZ>
-	friend class Sprites;
+/* ——————————————————————————————————————————————————————————————————————
+	Sprites are ghostly images which hover above a regular video image.
 
+	Displaying sprites is quite cpu intensive.  
+	Eventually the most popular use of a sprite is the display of a mouse pointer.  
+	Sprites don't take ownership of the shape => they don't delete it in ~Sprite().  
+
+	Shape must provide:
+		animated
+		width, height, hot_x, hot_y
+		typedef Shape
+	HotShape must provide:
+		skip_row()
+		render_row()
+*/
+
+
+template<typename SHAPE>
+class Sprite : public LinkedListElement
+{
 public:
 	NO_COPY_MOVE(Sprite);
 
-	using Shape = SHAPE;
-	template<ZPlane WZ>
-	using HotShape				   = typename Shape::template HotShape<WZ>;
-	static constexpr bool animated = Shape::animated;
-	static_assert(!animated);
+	using Shape	 = SHAPE;
+	using Frame	 = Video::Frame<Shape>;
+	using Frames = Video::Frames<Shape>;
 
-	Sprite(Shape s, coord x, coord y, uint16 z = 0) noexcept : // ctor
-		shape(s),
-		pos_x(x - s.hot_x()),
-		pos_y(y - s.hot_y()),
-		z(z)
-	{}
-	Sprite(Shape s, const Point& p, uint16 z = 0) noexcept : // ctor
-		Sprite(s, p.x, p.y, z)
-	{}
+	static constexpr bool is_animated = false;
+	static constexpr bool isa_sprite  = true;
+	static_assert(Shape::isa_shape);
+
+	Sprite(const Shape& s, const Point& p, uint16 z = 0) noexcept : shape(s), pos(p - s.hotspot()), z(z) {}
+	Sprite(Shape&& s, const Point& p, uint16 z = 0) noexcept : shape(std::move(s)), pos(p - s.hotspot()), z(z) {}
+
+	// the following ctors are mostly to support templates:
+	Sprite(const Frames& frames, const Point& p, uint16 z = 0) noexcept : Sprite(frames[0].shape, p, z) {}
+	Sprite(Frames&& frames, const Point& p, uint16 z = 0) noexcept : Sprite(frames[0].shape, p, z) {}
+	Sprite(const Frame* frames, uint8, const Point& p, uint16 z = 0) noexcept : Sprite(frames[0].shape, p, z) {}
+	Sprite(const Shape* s, const uint16*, uint8, const Point& p, uint16 z = 0) noexcept : Sprite(s[0], p, z) {}
+	Sprite(const Shape* shapes, uint16, uint8, const Point& p, uint16 z = 0) noexcept : Sprite(shapes[0], p, z) {}
+
 	~Sprite() noexcept = default;
 
-	int width() const noexcept { return shape.width(); }
-	int height() const noexcept { return shape.height(); }
-	int hot_x() const noexcept { return shape.hot_x(); }
-	int hot_y() const noexcept { return shape.hot_y(); }
+	int	 width() const noexcept { return shape.width(); }
+	int	 height() const noexcept { return shape.height(); }
+	int	 hot_x() const noexcept { return shape.hot_x(); }
+	int	 hot_y() const noexcept { return shape.hot_y(); }
+	Size size() const noexcept { return shape.size(); }
+	Dist hotspot() const noexcept { return shape.hotspot(); }
 
-	coord get_xpos() const noexcept { return pos_x + hot_x(); }
-	coord get_ypos() const noexcept { return pos_y + hot_y(); }
-	void  set_xpos(int x) noexcept { pos_x = x - hot_x(); }
-	void  set_ypos(int y) noexcept { pos_y = y - hot_y(); }
+	coord get_xpos() const noexcept { return pos.x + hot_x(); }
+	coord get_ypos() const noexcept { return pos.y + hot_y(); }
+	void  set_xpos(int x) noexcept { pos.x = x - hot_x(); }
+	void  set_ypos(int y) noexcept { pos.y = y - hot_y(); }
 
-	Size  size() const noexcept { return Size(width(), height()); }
-	Dist  hotspot() const noexcept { return Dist(hot_x(), hot_y()); }
-	Point get_position() const noexcept { return Point(pos_x + hot_x(), pos_y + hot_y()); }
-	void  set_position(coord x, coord y) noexcept
+	Point get_position() const noexcept { return pos + hotspot(); }
+	void  set_position(const Point& p) noexcept { pos = p - hotspot(); }
+
+	bool replace(const Shape& new_shape) noexcept
 	{
-		pos_x = x - hot_x();
-		pos_y = y - hot_y();
-	}
-	void set_position(const Point& p) noexcept { set_position(p.x, p.y); }
+		// returns true if Sprite may need to be re-linked
 
-	bool replace(const Shape& new_shape) noexcept // may also need re-linking
-	{
-		int dx = shape.hot_x() - new_shape.hot_x();
-		int dy = shape.hot_y() - new_shape.hot_y();
+		Dist d = shape.hotspot() - new_shape.hotspot();
 		shape  = new_shape;
-		pos_x += dx;
-		pos_y += dy;
-		return dy != 0;
-	}
-	void		  next_frame() noexcept {}
-	constexpr int num_frames() const noexcept { return 1; }
-
-	bool is_hot() const noexcept { return hot_row >= pos_y && hot_row < pos_y + height(); }
-	void wait_while_hot() const noexcept
-	{
-		while (is_hot()) sleep_us(500);
+		pos += d;
+		return d.dy != 0;
 	}
 
-	template<ZPlane WZ>
-	void setup(HotShape<WZ>& hs) const noexcept
+	template<typename HotShape>
+	void start(HotShape& hs) const noexcept
 	{
-		hs.pixels  = &shape.pixels[0];
-		hs.x	   = pos_x;
-		hs.ghostly = ghostly;
-		if constexpr (WZ == HasZ) hs.z = z;
+		shape.start(hs, pos.x, ghostly);
 	}
 
 	Shape shape; // the compressed image of the sprite
-	coord pos_x;
-	coord pos_y;
+	Point pos;
 
-	uint16 z;				  // if HasZ
-	bool   ghostly	 = false; // translucent
-	uint8  frame_idx = 0;	  // if animated
+	uint16 z;					  // if HasZ
+	bool   ghostly		 = false; // translucent
+	uint8  current_frame = 0;	  // if animated
 };
 
-
-/* ————————————————————————————————————————————————————————————
-	Animated Sprite
-	the animated Sprite is based on the not-animated Sprite.
-*/
-template<typename SHAPE>
-class Sprite<SHAPE, typename std::enable_if_t<SHAPE::animated>> : public Sprite<typename SHAPE::Shape>
-{
-	template<typename Sprite, ZPlane WZ>
-	friend class Sprites;
-
-public:
-	using super = Sprite<typename SHAPE::Shape>;
-	using Shape = SHAPE;
-
-	static constexpr bool animated = true;
-
-	using super::frame_idx;
-
-	Shape animated_shape;
-	int	  countdown;
-
-	Sprite(Shape shape, const Point& p, uint16 z = 0) noexcept : // ctor
-		super(shape[0].shape, p, z),
-		animated_shape(std::move(shape)),
-		countdown(shape[0].duration)
-	{}
-	~Sprite() noexcept = default;
-
-	int num_frames() const noexcept { return animated_shape.num_frames; }
-
-	bool replace(Shape q) noexcept // may also need re-linking
-	{
-		animated_shape = std::move(q);
-		return super::replace(q[0].shape);
-	}
-
-	bool next_frame() noexcept // may also need re-linking
-	{
-		if constexpr (animated)
-		{
-			if (++frame_idx >= num_frames()) frame_idx = 0;
-			countdown = animated_shape[frame_idx].duration;
-			return super::replace(animated_shape[frame_idx].shape);
-		}
-	}
-};
 
 } // namespace kio::Video
 
