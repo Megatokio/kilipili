71818a1989b6add8dc9951f85993145ef5a9d08f
kio <kio@little-bat.de>
2023-12-31 20:57:06 +0100

    copyright year

--- common/sort.h
+++ common/sort.h
@@ -1,5 +1,5 @@
 #pragma once
-// Copyright (c) 1998 - 2023 kio@little-bat.de
+// Copyright (c) 1998 - 2024 kio@little-bat.de
 // BSD-2-Clause license
 // https://opensource.org/licenses/BSD-2-Clause
 
