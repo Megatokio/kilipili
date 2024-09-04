bc5e1c78b2f0cce91050b9b5a6a7f3476442807b
kio <kio@little-bat.de>
2024-09-04 10:03:05 +0200

    PixmapMockup

---
+++ Graphics/PixmapMockup.cpp
@@ -0,0 +1,97 @@
+// Copyright (c) 2024 - 2024 kio@little-bat.de
+// BSD-2-Clause license
+// https://opensource.org/licenses/BSD-2-Clause
+
+#include "PixmapMockup.h"
+#include "common/cstrings.h"
+
+namespace kio::Graphics
+{
+
+PixmapMockup::PixmapMockup(coord w, coord h, ColorMode CM, AttrHeight AH) : Canvas(w, h, CM, AH, false)
+{
+	log.append(usingstr("%s(%i,%i,%s,%s)", __func__, w, h, tostr(CM), tostr(AH)));
+}
+
+Canvas* PixmapMockup::cloneWindow(coord x, coord y, coord w, coord h) throws
+{
+	log.append(usingstr("%s(%i,%i,%i,%i)", __func__, x, y, w, h));
+	return nullptr; // TODO();
+}
+
+void PixmapMockup::set_pixel(coord x, coord y, uint color, uint ink) noexcept
+{
+	log.append(usingstr("%s(%i,%i,%u,%u)", __func__, x, y, color, ink));
+}
+
+uint PixmapMockup::get_pixel(coord x, coord y, uint* ink) const noexcept
+{
+	log.append(usingstr("%s(%i,%i,uint*)", __func__, x, y)); //
+	if (ink) *ink = 0;
+	return 0;
+}
+
+uint PixmapMockup::get_color(coord x, coord y) const noexcept
+{
+	log.append(usingstr("%s(%i,%i)", __func__, x, y)); //
+	return 0;
+}
+
+uint PixmapMockup::get_ink(coord x, coord y) const noexcept
+{
+	log.append(usingstr("%s(%i,%i)", __func__, x, y)); //
+	return 0;
+}
+
+void PixmapMockup::draw_hline_to(coord x, coord y, coord x2, uint color, uint ink) noexcept
+{
+	log.append(usingstr("%s(%i,%i,%i,%u,%u)", __func__, x, y, x2, color, ink));
+}
+
+void PixmapMockup::draw_vline_to(coord x, coord y, coord y2, uint color, uint ink) noexcept
+{
+	log.append(usingstr("%s(%i,%i,%i,%u,%u)", __func__, x, y, y2, color, ink));
+}
+
+void PixmapMockup::fillRect(coord x, coord y, coord w, coord h, uint color, uint ink) noexcept
+{
+	log.append(usingstr("%s(%i,%i,%i,%i,%u,%u)", __func__, x, y, w, h, color, ink));
+}
+
+void PixmapMockup::xorRect(coord x, coord y, coord w, coord h, uint color) noexcept
+{
+	log.append(usingstr("%s(%i,%i,%i,%i,%u)", __func__, x, y, w, h, color));
+}
+
+void PixmapMockup::clear(uint color) noexcept
+{
+	log.append(usingstr("%s(%u)", __func__, color)); //
+}
+
+void PixmapMockup::copyRect(coord zx, coord zy, coord qx, coord qy, coord w, coord h) noexcept
+{
+	log.append(usingstr("%s(%i,%i,%i,%i,%i,%i)", __func__, zx, zy, qx, qy, w, h));
+}
+
+void PixmapMockup::copyRect(coord zx, coord zy, const Canvas&, coord qx, coord qy, coord w, coord h) noexcept
+{
+	log.append(usingstr("%s(%i,%i,Canvas,%i,%i,%i,%i)", __func__, zx, zy, qx, qy, w, h));
+}
+
+void PixmapMockup::readBmp(coord x, coord y, uint8*, int row_offs, coord w, coord h, uint color, bool set) noexcept
+{
+	log.append(usingstr("%s(%i,%i,uint8*,%i,%i,%i,%u,%u)", __func__, row_offs, x, y, w, h, color, set));
+}
+
+void PixmapMockup::drawBmp(coord zx, coord zy, const uint8*, int ro, coord w, coord h, uint color, uint ink) noexcept
+{
+	log.append(usingstr("%s(%i,%i,bmp,%i,%i,%i,%u,%u)", __func__, ro, zx, zy, w, h, color, ink));
+}
+
+void PixmapMockup::drawChar(coord zx, coord zy, const uint8*, coord h, uint color, uint ink) noexcept
+{
+	log.append(usingstr("%s(%i,%i,bmp,%i,%u,%u)", __func__, zx, zy, h, color, ink));
+}
+
+
+} // namespace kio::Graphics
---
+++ Graphics/PixmapMockup.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2024 - 2024 kio@little-bat.de
+// BSD-2-Clause license
+// https://opensource.org/licenses/BSD-2-Clause
+
+#pragma once
+#include "Canvas.h"
+#include "Video/video_types.h"
+#include "common/Array.h"
+
+namespace kio::Graphics
+{
+
+class PixmapMockup : public Canvas
+{
+public:
+	PixmapMockup(coord w, coord h, ColorMode CM = colormode_a1w8_rgb, AttrHeight AH = attrheight_12px);
+	~PixmapMockup() override = default;
+
+	Canvas* cloneWindow(coord x, coord y, coord w, coord h) throws override;
+	void	set_pixel(coord x, coord y, uint color, uint ink = 0) noexcept override;
+	uint	get_pixel(coord x, coord y, uint* ink) const noexcept override;
+	uint	get_color(coord x, coord y) const noexcept override;
+	uint	get_ink(coord x, coord y) const noexcept override;
+	void	draw_hline_to(coord x, coord y, coord x2, uint color, uint ink) noexcept override;
+	void	draw_vline_to(coord x, coord y, coord y2, uint color, uint ink) noexcept override;
+	void	fillRect(coord x, coord y, coord w, coord h, uint color, uint ink = 0) noexcept override;
+	void	xorRect(coord x, coord y, coord w, coord h, uint color) noexcept override;
+	void	clear(uint color) noexcept override;
+	void	copyRect(coord zx, coord zy, coord qx, coord qy, coord w, coord h) noexcept override;
+	void	copyRect(coord zx, coord zy, const Canvas& src, coord qx, coord qy, coord w, coord h) noexcept override;
+	void readBmp(coord x, coord y, uint8* bmp, int row_offs, coord w, coord h, uint color, bool set) noexcept override;
+	void drawBmp(coord zx, coord zy, const uint8* bmp, int ro, coord w, coord h, uint c, uint i = 0) noexcept override;
+	void drawChar(coord zx, coord zy, const uint8* bmp, coord h, uint color, uint = 0) noexcept override;
+
+	mutable Array<cstr> log; // tempstr
+};
+
+} // namespace kio::Graphics
