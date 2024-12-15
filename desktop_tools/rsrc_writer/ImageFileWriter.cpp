7bbac1495ce1095c7d0247a0ca6bbbe1e49b15e3
kio <kio@little-bat.de>
2024-12-15 16:53:11 +0100

    StdFile: move from desktop_tools to Devices

--- desktop_tools/rsrc_writer/ImageFileWriter.cpp
+++ desktop_tools/rsrc_writer/ImageFileWriter.cpp
@@ -3,7 +3,7 @@
 // https://opensource.org/licenses/BSD-2-Clause
 
 #include "ImageFileWriter.h"
-#include "StdFile.h"
+#include "Devices/StdFile.h"
 #include "cstrings.h"
 #include PICO_BOARD
 #include "Devices/HeatShrinkEncoder.h"
--- desktop_tools/rsrc_writer/YMFileConverter.h
+++ desktop_tools/rsrc_writer/YMFileConverter.h
@@ -3,7 +3,7 @@
 // https://opensource.org/licenses/BSD-2-Clause
 
 #pragma once
-#include "StdFile.h"
+#include "Devices/StdFile.h"
 #include "standard_types.h"
 #include <cstdio>
 
