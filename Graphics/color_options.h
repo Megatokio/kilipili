5014b826855e061bff9f89f35913a47068987cf3
kio <kio@little-bat.de>
2025-08-04 19:42:13 +0200

    update for pico-sdk v2.2

--- Graphics/color_options.h
+++ Graphics/color_options.h
@@ -5,7 +5,8 @@
 
 // pull in board description file for targeted board:
 #ifndef MAKE_TOOLS
-  #include <pico/config.h>
+//#include <pico/config.h>
+  #include <pico.h>
 #elif defined PICO_BOARD_H
   #include PICO_BOARD_H
 #endif
