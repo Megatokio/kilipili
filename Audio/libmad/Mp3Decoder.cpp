d2c04161488dd37b16cb471e933d38c2e037dfba
kio <kio@little-bat.de>
2026-09-14 11:05:10 +0200

    Mp3Decoder (ctd.)

--- Audio/libmad/Mp3Decoder.cpp
+++ Audio/libmad/Mp3Decoder.cpp
@@ -3,11 +3,10 @@
 // https://opensource.org/licenses/BSD-2-Clause
 
 #include "Mp3Decoder.h"
+#include "common/RCPtr.h"
 #include "common/cdefs.h"
 #include "common/standard_types.h"
 #include "common/trace.h"
-//#include "global.h"
-#include "common/RCPtr.h"
 #include <cstdio>
 #include <cstring>
 #include <new>
@@ -15,246 +14,88 @@
 namespace kilipili::Audio
 {
 
-void Mp3Player::filter(const MadStream*, MadFrame*) {}
+void Mp3Decoder::filter(const MadStream*, MadFrame*) {}
 
-Error Mp3Player::play(int stream_options) noexcept
-{
-	RCPtr<MadSynth> synth(new MadSynth);	// too big: > 13 kB
-	RCPtr<MadFrame> frame(new MadFrame);	// too big: > 9 kB
-	MadStream		stream(stream_options); // ~ 100 byte
-	constexpr uint	buffer_size = 2000;
-	uint			buffer_count {0};
-
-	struct Buffer
-	{
-		uchar data[buffer_size]; // input stream buffer
-		int	  rc = 0;
-	};
-
-	RCPtr<Buffer> buffer(new Buffer);
-
-	//debugstr("mp3_input: first call\n");
-	buffer_count = input(buffer->data, buffer_size);
-	//debugstr("mp3_input: read %u bytes\n", buffer_count);
-	assert(buffer_count != 0);
-	stream.set_buffer_pointers(buffer->data, buffer_count);
-
-	for (;;)
-	{
-		for (;;)
-		{
-			if (debug && frame->header.decode(&stream) == -1)
-			{
-				if (stream.error == MAD_ERROR_BUFLEN) break;
-				Error error = stream.errorstr();
-				debugstr("mp3_header_decode: %s\n", error);
-				if (!is_recoverable(stream.error)) return error;
-				else continue; // goto skip;
-			}
-
-			if (frame->decode(&stream) == -1)
-			{
-				if (stream.error == MAD_ERROR_BUFLEN) break;
-				Error error = stream.errorstr();
-				debugstr("mp3_frame_decode: %s\n", error);
-				if (!is_recoverable(stream.error)) return error;
-				else continue; // goto skip;
-			}
-
-			filter(&stream, frame);
-			synth->synthesize_pcm(frame);
-			output(&frame->header, &synth->pcm);
-		}
-
-		//skip:
-		assert(stream.error == MAD_ERROR_BUFLEN);
-		int nprocessed = stream.next_frame - buffer->data; // num bytes decoder processed for current (last) frame
-		int nremaining = buffer_count - nprocessed;		   // num bytes remaining unprocessed in buffer
-		assert(nprocessed >= 0 && nprocessed <= buffer_count);
-		assert(nremaining >= 0 && nremaining <= buffer_count);
-		if unlikely (nremaining == buffer_size) return "mp3_input: buffer too small";
-		memmove(buffer->data, stream.next_frame, nremaining); // move remaining data to start of buffer
-
-		uint n		 = input(buffer->data + nremaining, buffer_size - nremaining);
-		buffer_count = nremaining + n;
-
-		if (n)
-		{
-			//debugstr("mp3_input: read %u bytes\n", n);
-			stream.set_buffer_pointers(buffer->data, nremaining + n);
-		}
-		else // n=0 -> eof
-		{
-			debugstr("mp3_input: %u bytes not processed at eof\n", nremaining);
-			return nullptr;
-		}
-	}
-}
-
-
-// --------------------------------------------------------
-#if 0
-
-Mp3Decoder::Mp3Decoder() noexcept {}
-Mp3Decoder::~Mp3Decoder() noexcept {}
-
-
-//	Mp3Player::FlowCtl Mp3Player::header(const MadHeader*)
-//	{
-//		f_header = 0; // don't call again: subclass didn't implement header()
-//		return CONTINUE;
-//	}
-//void Mp3Player::filter(const MadStream*, MadFrame*)
-//{
-//	f_filter = 0; // don't call again: subclass didn't implement filter()
-//}
-
-mad_flow Mp3Decoder::handle_error(MadStream* stream, struct MadFrame* frame)
-{
-	// default error action:
-
-	switch (stream->error)
-	{
-	case MAD_ERROR_BADCRC: MadFrame_mute(frame); return MAD_FLOW_IGNORE;
-	default: return MAD_FLOW_CONTINUE;
-	}
-}
-mad_flow Mp3Decoder::handle_header(const MadHeader*)
-{
-	f_header = 0; // don't call again: subclass didn't implement handle_header()
-	return MAD_FLOW_CONTINUE;
-}
-mad_flow Mp3Decoder::filter(const MadStream*, MadFrame*)
-{
-	f_filter = 0; // don't call again: subclass didn't implement filter()
-	return MAD_FLOW_CONTINUE;
-}
-
-//void Mp3Decoder::set_options(int options) noexcept //
-//{
-//	MadStream_options(this->stream, options);
-//}
-
-int Mp3Decoder::run(int options)
+Error Mp3Decoder::play(int stream_options) noexcept
 {
 	trace(__func__);
 
-	this->options = options;
-
-	struct Sync
+	try
 	{
-		mutable int rc = 0; // for RCPtr
-		MadStream	stream;
-		MadFrame	frame;
-		MadSynth	synth;
-	};
+		RCPtr<MadSynth> synth(new MadSynth);	// too big: > 13 kB
+		RCPtr<MadFrame> frame(new MadFrame);	// too big: > 9 kB
+		MadStream		stream(stream_options); // ~ 100 byte
+		constexpr uint	buffer_size	 = 2000;
+		uint			buffer_count = 0;
 
-	RCPtr<Sync> sync {new (std::nothrow) Sync};
-	if (sync == nullptr) return -1;
-
-	MadStream* stream = &sync->stream;
-	MadFrame*  frame  = &sync->frame;
-	MadSynth*  synth  = &sync->synth;
-	int		   result = 0;
-
-	MadStream_options(stream, this->options);
-
-	do {
-		switch (input(stream))
+		struct Buffer
 		{
-		case MAD_FLOW_STOP: goto done;
-		case MAD_FLOW_BREAK: goto fail;
-		case MAD_FLOW_IGNORE: continue;
-		case MAD_FLOW_CONTINUE: break;
-		default: break;
-		}
+			uchar data[buffer_size]; // input stream buffer
+			int	  rc = 0;
+		};
 
-		while (1)
+		RCPtr<Buffer> buffer(new Buffer);
+
+		//debugstr("mp3_input: first call\n");
+		buffer_count = input(buffer->data, buffer_size);
+		//debugstr("mp3_input: read %u bytes\n", buffer_count);
+		assert(buffer_count != 0);
+		stream.set_buffer_pointers(buffer->data, buffer_count);
+
+		for (;;)
 		{
-			// if (f_header)
-			// {
-			// 	if (frame->header.decode( stream) == -1)
-			// 	{
-			// 		cstr msg = MadStream_errorstr(stream);
-			// 		debugstr("mp3_header_decode: %s\n", msg);
-			// 		if (!MAD_RECOVERABLE(stream->error)) return msg;
-			// 		else goto skip;
-			// 	}
-
-			// 	FlowCtl flow = header(&frame->header);
-			// 	if (flow == SKIP) goto skip;
-			// 	if (flow == STOP) return nullptr;
-			// }
-
-			if (f_header)
+			for (;;)
 			{
-				if (frame->header.decode(stream) == -1)
+				if (debug && frame->header.decode(&stream) == -1)
 				{
-					if (!MAD_RECOVERABLE(stream->error)) break;
-
-					switch (handle_error(stream, frame))
-					{
-					case MAD_FLOW_STOP: goto done;
-					case MAD_FLOW_BREAK: goto fail;
-					case MAD_FLOW_IGNORE:
-					case MAD_FLOW_CONTINUE:
-					default: continue;
-					}
+					if (stream.error == MAD_ERROR_BUFLEN) break;
+					debugstr("mp3_header_decode: %s\n", stream.errorstr());
+					if (is_recoverable(stream.error)) continue;
+					else return stream.errorstr();
 				}
 
-				switch (handle_header(&frame->header))
+				if (frame->decode(&stream) == -1)
 				{
-				case MAD_FLOW_STOP: goto done;
-				case MAD_FLOW_BREAK: goto fail;
-				case MAD_FLOW_IGNORE: continue;
-				case MAD_FLOW_CONTINUE: break;
+					if (stream.error == MAD_ERROR_BUFLEN) break;
+					debugstr("mp3_frame_decode: %s\n", stream.errorstr());
+					if (is_recoverable(stream.error)) continue;
+					else return stream.errorstr();
 				}
+
+				filter(&stream, frame);
+				synth->synthesize_pcm(frame);
+				output(&frame->header, &synth->pcm);
 			}
 
-			if (MadFrame_decode(frame, stream) == -1)
-			{
-				if (!MAD_RECOVERABLE(stream->error)) break;
+			//skip:
+			assert(stream.error == MAD_ERROR_BUFLEN);
+			int nprocessed = stream.next_frame - buffer->data; // num bytes decoder processed for current (last) frame
+			int nremaining = buffer_count - nprocessed;		   // num bytes remaining unprocessed in buffer
+			assert(nprocessed >= 0 && nprocessed <= buffer_count);
+			assert(nremaining >= 0 && nremaining <= buffer_count);
+			if unlikely (nremaining == buffer_size) return "mp3_input: buffer too small";
+			memmove(buffer->data, stream.next_frame, nremaining); // move remaining data to start of buffer
 
-				switch (handle_error(stream, frame))
-				{
-				case MAD_FLOW_STOP: goto done;
-				case MAD_FLOW_BREAK: goto fail;
-				case MAD_FLOW_IGNORE: break;
-				case MAD_FLOW_CONTINUE: continue;
-				}
+			uint n		 = input(buffer->data + nremaining, buffer_size - nremaining);
+			buffer_count = nremaining + n;
+
+			if (n)
+			{
+				//debugstr("mp3_input: read %u bytes\n", n);
+				stream.set_buffer_pointers(buffer->data, nremaining + n);
 			}
-
-			if (f_filter)
+			else // n=0 -> eof
 			{
-				switch (filter(stream, frame))
-				{
-				case MAD_FLOW_STOP: goto done;
-				case MAD_FLOW_BREAK: goto fail;
-				case MAD_FLOW_IGNORE: continue;
-				case MAD_FLOW_CONTINUE: break;
-				}
-			}
-
-			MadSynth_frame(synth, frame);
-
-			switch (output(&frame->header, &synth->pcm))
-			{
-			case MAD_FLOW_STOP: goto done;
-			case MAD_FLOW_BREAK: goto fail;
-			case MAD_FLOW_IGNORE:
-			case MAD_FLOW_CONTINUE: break;
+				debugstr("mp3_input: %u bytes not processed at eof\n", nremaining);
+				return nullptr;
 			}
 		}
 	}
-	while (stream->error == MAD_ERROR_BUFLEN);
-
-fail:
-	result = -1;
-
-done:
-	return result;
+	catch (Error e)
+	{
+		return e;
+	}
 }
-#endif
 
 } // namespace kilipili::Audio
 
--- Audio/libmad/Mp3Decoder.h
+++ Audio/libmad/Mp3Decoder.h
@@ -3,7 +3,6 @@
 // https://opensource.org/licenses/BSD-2-Clause
 
 #pragma once
-//#include "common/RCPtr.h"
 #include "MadFrame.h"
 #include "MadStream.h"
 #include "MadSynth.h"
@@ -13,14 +12,11 @@
 namespace kilipili::Audio
 {
 
-
-class Mp3Player
+class Mp3Decoder
 {
 public:
-	//	enum FlowCtl { CONTINUE = 0, SKIP = 1, STOP = 2 };
-
-	Mp3Player() noexcept		  = default;
-	virtual ~Mp3Player() noexcept = default;
+	Mp3Decoder() noexcept		   = default;
+	virtual ~Mp3Decoder() noexcept = default;
 
 	Error play(int stream_options = 0) noexcept;
 
@@ -30,47 +26,13 @@ public:
 	*/
 	virtual uint input(uchar* data, uint count) = 0;
 
-	//	/*	optional callback: determine whether this block should be played.
-	//		return: CONTINUE = play it (normal), SKIP = skip block, STOP = return from play()
-	//	*/
-	//	virtual FlowCtl header(const MadHeader*);
-
-	virtual void filter(const MadStream*, MadFrame*); // optional
+	/*	callback: apply filter (optional)
+	*/
+	virtual void filter(const MadStream*, MadFrame*);
 
 	/*	callback: send data to audio output
 	*/
 	virtual void output(const MadHeader*, const MadPcmBuffer*) = 0;
-
-	//bool f_header {1};
-	//bool f_filter {1};
 };
-
-//	enum mad_flow {
-//		MAD_FLOW_CONTINUE = 0x0000, /* continue normally */
-//		MAD_FLOW_STOP	  = 0x0010, /* stop decoding normally */
-//		MAD_FLOW_BREAK	  = 0x0011, /* stop decoding and signal an error */
-//		MAD_FLOW_IGNORE	  = 0x0020, /* ignore (mute) the current frame */
-//	};
-//
-//	class Mp3Decoder
-//	{
-//	public:
-//		Mp3Decoder() noexcept;
-//		virtual ~Mp3Decoder() noexcept;
-//
-//		void set_options(int options) noexcept;
-//		int	 run(int options = 0);
-//
-//		virtual mad_flow input(MadStream*) = 0;						  // input more data to MadStream
-//		virtual mad_flow handle_header(const MadHeader*);			  // optional
-//		virtual mad_flow filter(const MadStream*, MadFrame*);		  // optional
-//		virtual mad_flow output(const MadHeader*, MadPcmBuffer*) = 0; // output samples from MadPcmBuffer
-//		virtual mad_flow handle_error(MadStream*, MadFrame*);		  // decode header or decode frame failed
-//
-//		int	 options {0};
-//		bool f_header {1};
-//		bool f_filter {1};
-//	};
-
 
 } // namespace kilipili::Audio
